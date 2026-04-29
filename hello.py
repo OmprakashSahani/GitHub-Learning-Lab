e30fc9542a2ba2ea26c64025f56f0a8e92859703
Omprakash Sahani <Omprakash.Sahani7991@gmail.com>
2026-04-29 13:11:05 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def greet(name: str) -> str:
-    return f"Hello, {name}! Welcome to GitHub 🚀"
-
-if __name__ == "__main__":
-    name = sys.argv[1] if len(sys.argv) > 1 else "Omprakash"
-    print(greet(name))
