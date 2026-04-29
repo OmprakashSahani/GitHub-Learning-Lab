80edeeaa0e989ce91f6f9fc98abfc0336773a812
Omprakash Sahani <Omprakash.Sahani7991@gmail.com>
2026-04-29 12:33:03 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
+import sys
+
 def greet(name: str) -> str:
     return f"Hello, {name}! Welcome to GitHub."
 
 if __name__ == "__main__":
-    print(greet("Omprakash Sahani"))
+    name = sys.argv[1] if len(sys.argv) > 1 else "Omprakash"
+    print(greet(name))
